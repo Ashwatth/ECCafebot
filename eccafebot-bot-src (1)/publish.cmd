01b71b3c3eae4978a8e4dd63d2a27ca4cfb26e05
Ashwatth <92110686+Ashwatth@users.noreply.github.com>
2022-04-01 19:18:38 +0530

    all eccafe bot files unzipped files

---
+++ eccafebot-bot-src (1)/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\eccafebot-bot-ba05-Web-Deploy.pubxml -p:Password=DGPnlgN2mtGaLanhfJ8nyK48w1Xevs2xjacH1kZnllqoi21Z81iR0vnlzgvF
+
